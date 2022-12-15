c689eab3d8f8c76cb6f05f09ee97ad6fbda0d978
guwdron23 <bagitov@gmail.com>
2022-12-15 12:56:52 +0600

    all

--- news/models.py
+++ news/models.py
@@ -1,3 +1,23 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор')
+    title = models.CharField(max_length=200, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    created_date = models.DateTimeField(default=timezone.now, verbose_name='Дата создание')
+    published_date = models.DateTimeField(blank=True, null=True, verbose_name='Дата публикации')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
